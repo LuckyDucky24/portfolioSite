d5b45d8e8b78d43c3cbdb0184ab387b4303326ec
Sean Nam <sean.nam01@gmail.com>
2017-08-25 16:36:22 -0700

    fixed background

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-responsive-images');
 
 
     grunt.initConfig({
@@ -12,8 +13,16 @@ module.exports = function(grunt) {
         watch: {
             files: ['Gruntfile.js'],
             tasks: ['jshint']
-        }
-    });
+        },
+        responsive_images: {
+            options: {
+                // Task-specific options go here
+            },
+            your_target: {
+                // Target-specific file lists and/or options go here.
+            },
+        },
+    })
 
 
     // task setup
